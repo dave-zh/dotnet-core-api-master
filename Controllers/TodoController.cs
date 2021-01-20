9e83dececc10f4cb7f3855232c8602fce909254a
leo <leo@DEV01>
2021-01-20 16:20:15 +0100

    x

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -23,7 +23,7 @@ namespace TodoApi.Controllers
 
             if (_context.TodoItems.Count() == 0)
             {
-                _context.TodoItems.Add(new TodoItem { Name = "Item11" });
+                _context.TodoItems.Add(new TodoItem { Name = "Item13" });
                 _context.SaveChanges();
             }
         }
